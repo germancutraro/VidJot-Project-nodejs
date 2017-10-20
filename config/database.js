2780e2a43149fe5226d5a17babb630f832d71a27
Germán <germancutraro@hotmail.com>
2017-10-20 19:10:09 -0300

    Test env

--- config/database.js
+++ config/database.js
@@ -1,8 +1,6 @@
-// var environment = process.env.NODE_ENV || 'production';
-// if (environment === 'production') {
-//   module.exports = {url: 'mongodb://germancutraro:sherman@ds013172.mlab.com:13172/vidjot-production'};
-// } else {
-//   module.exports = {url: 'mongodb://localhost/vidjot'};
-// }
-
-module.exports = {url: 'mongodb://germancutraro:sherman@ds013172.mlab.com:13172/vidjot-production'};
+var environment = process.env.NODE_ENV || 'production';
+if (environment === 'production') {
+  module.exports = {url: 'mongodb://germancutraro:sherman@ds013172.mlab.com:13172/vidjot-production'};
+} else {
+  module.exports = {url: 'mongodb://localhost/vidjot'};
+}
