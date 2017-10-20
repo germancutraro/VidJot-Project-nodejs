4f1cc93f133c59f2b08bcba1da200c7627fef557
Germán <germancutraro@hotmail.com>
2017-10-20 19:08:57 -0300

    Initial Commit

--- config/database.js
+++ config/database.js
@@ -1,5 +1,8 @@
-if (process.env.NODE_ENV === "'production'") {
-  module.exports = {url: 'mongodb://germancutraro:sherman@ds013172.mlab.com:13172/vidjot-production'};
-} else {
-  module.exports = {url: 'mongodb://localhost/vidjot'};
-}
+// var environment = process.env.NODE_ENV || 'production';
+// if (environment === 'production') {
+//   module.exports = {url: 'mongodb://germancutraro:sherman@ds013172.mlab.com:13172/vidjot-production'};
+// } else {
+//   module.exports = {url: 'mongodb://localhost/vidjot'};
+// }
+
+module.exports = {url: 'mongodb://germancutraro:sherman@ds013172.mlab.com:13172/vidjot-production'};
