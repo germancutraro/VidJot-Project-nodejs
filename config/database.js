e43afcae2c834594ba174a5e7260454c3df306fa
Germán <germancutraro@hotmail.com>
2017-10-20 22:02:54 -0300

    putting everything on time

--- config/database.js
+++ config/database.js
@@ -1,5 +1,4 @@
-if (process.env.NODE_ENV  === 'production') {
+if (process.env.NODE_ENV  === 'production')
   module.exports = {url: 'mongodb://germancutraro:sherman@ds013172.mlab.com:13172/vidjot-production'};
-} else {
+else
   module.exports = {url: 'mongodb://localhost/vidjot'};
-}
--- libs/db-connection.js
+++ libs/db-connection.js
@@ -9,6 +9,6 @@ mongoose.connect(MONGO_URL, {useMongoClient: true});
 mongoose.connection
   .once('open', () => console.log('Connected to the database!'))
   .on('error', err => console.log(err));
-console.log(`SHEEEEEERMAN ${process.env.NODE_ENV}`);
+
 console.log(MONGO_URL)
 module.exports = MONGO_URL;
